cf642299a11bba90f88a105ba531964b69148dce
Luke Cann <luke@lc-apps.co.uk>
2023-08-14 21:57:39 +0100

    Name & Danger level in editor

--- tools/editor/EditorMain.hx
+++ tools/editor/EditorMain.hx
@@ -5,20 +5,25 @@ import js.Browser;
 import js.html.Blob;
 import js.html.CanvasElement;
 import js.html.CanvasRenderingContext2D;
-import js.html.FileSystem;
+import js.html.DivElement;
 import js.html.ImageElement;
 import js.html.InputElement;
 import js.html.InputEvent;
 import js.html.MouseEvent;
 import js.html.SelectElement;
 import js.html.URL;
-import js.html.Window;
 
 class EditorMain {
+	private static inline var SCALE = 3.6;
+
 	public static var canvas(default, null):CanvasElement;
 	public static var context(default, null):CanvasRenderingContext2D;
 	public static var map(default, null):ImageElement;
 	public static var mode(default, null):SelectElement;
+	public static var locationOptions(default, null):DivElement;
+	public static var routeOptions(default, null):DivElement;
+	public static var inputName(default, null):InputElement;
+	public static var inputDanger(default, null):InputElement;
 
 	private static var locations = new Array<Location>();
 	private static var routes = new Array<Route>();
@@ -26,6 +31,9 @@ class EditorMain {
 	private static var start:Location = null;
 	private static var mouseX:Float = 0;
 	private static var mouseY:Float = 0;
+
+	private static var selectedLocation:Location = null;
+	private static var selectedRoute:Route = null;
 
 	public static function main() {
 		canvas = cast Browser.window.document.getElementById("c");
@@ -42,6 +50,18 @@ class EditorMain {
 
 		mode = cast Browser.window.document.getElementById("sel-mode");
 
+		locationOptions = cast Browser.window.document.getElementById("div-location");
+		routeOptions = cast Browser.window.document.getElementById("div-route");
+
+		inputName = cast Browser.window.document.getElementById("txt-name");
+		inputName.onchange = () -> {
+			selectedLocation.name = inputName.value;
+		}
+		inputDanger = cast Browser.window.document.getElementById("num-danger");
+		inputDanger.onchange = () -> {
+			selectedRoute.danger = inputDanger.valueAsNumber;
+		}
+
 		Browser.window.document.getElementById("btn-save").onclick = onSave;
 		Browser.window.document.getElementById("file-load").onchange = onLoad;
 	}
@@ -50,7 +70,7 @@ class EditorMain {
 		Browser.window.requestAnimationFrame(update);
 
 		context.clearRect(0, 0, canvas.width, canvas.height);
-		context.drawImage(map, 0, 0, 555, 1080);
+		context.drawImage(map, 0, 0, map.naturalWidth * SCALE, map.naturalHeight * SCALE);
 
 		for (l in locations) {
 			context.fillStyle = l.highlight ? "#FFFF00" : "#FF0000";
@@ -58,6 +78,10 @@ class EditorMain {
 			context.beginPath();
 			context.ellipse(l.x, l.y, 5, 5, 0, 0, Math.PI * 2);
 			context.fill();
+
+			context.fillStyle = "#000";
+			var w = context.measureText(l.name).width;
+			context.fillText(l.name, Math.round(l.x - w / 2), Math.round(l.y - 10));
 		}
 
 		context.strokeStyle = "2px solid #FF0000";
@@ -91,22 +115,28 @@ class EditorMain {
 
 	private static function onMouseUp(e:MouseEvent) {
 		if (mode.value == "add-town") {
-			locations.push({
+			selectedLocation = {
 				x: e.pageX,
 				y: e.pageY,
 				name: "newtown",
 				type: true,
 				highlight: false
-			});
+			};
+
+			locations.push(selectedLocation);
+			showOptions(true, false);
 		}
 		if (mode.value == "add-village") {
-			locations.push({
+			selectedLocation = {
 				x: e.pageX,
 				y: e.pageY,
 				name: "newville",
 				type: false,
 				highlight: false
-			});
+			};
+
+			locations.push(selectedLocation);
+			showOptions(true, false);
 		}
 		if (mode.value == "add-route" && start != null) {
 			for (l in locations) {
@@ -116,6 +146,15 @@ class EditorMain {
 				}
 			}
 			start = null;
+		}
+		if (mode.value == "select-location") {
+			for (l in locations) {
+				if (l.highlight) {
+					selectedLocation = l;
+					showOptions(true, false);
+					break;
+				}
+			}
 		}
 	}
 
@@ -136,7 +175,22 @@ class EditorMain {
 		});
 	}
 
+	private static inline function roundTo1(v:Float) {
+		return Math.round(v * 10) / 10;
+	}
+
 	private static function onSave() {
+		var scaledLocations = new Array<Location>();
+		for (l in locations) {
+			scaledLocations.push({
+				name: l.name,
+				type: l.type,
+				x: roundTo1(l.x / SCALE),
+				y: roundTo1(l.y / SCALE),
+				highlight: false
+			});
+		}
+
 		var resolvedRoutes = new Array<Dynamic>();
 		for (r in routes) {
 			resolvedRoutes.push({
@@ -147,7 +201,7 @@ class EditorMain {
 		}
 
 		var mapData = {
-			locations: locations,
+			locations: scaledLocations,
 			routes: resolvedRoutes
 		};
 
@@ -158,6 +212,23 @@ class EditorMain {
 
 	private static function onLoad(e:InputEvent) {
 		var ele:InputElement = cast e.target;
+	}
+
+	private static function showOptions(location:Bool, route:Bool) {
+		if (location) {
+			locationOptions.classList.remove("hide");
+			inputName.value = selectedLocation.name;
+		}
+		else {
+			locationOptions.classList.add("hide");
+		}
+		if (route) {
+			routeOptions.classList.remove("hide");
+			inputDanger.valueAsNumber = selectedRoute.danger;
+		}
+		else {
+			routeOptions.classList.add("hide");
+		}
 	}
 }
 
